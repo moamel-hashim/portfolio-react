2046bd8e8199aba5ea6947adf7b39b69e97f8b10
moamel97 <muamal1997@gmail.com>
2025-04-12 03:34:22 -0700

    editing some stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,13 +38,13 @@ export default function Home() {
 
       {/* Profile Section */}
       <section id="about" className="container d-flex align-items-center text-center my-5 justify-content-center gap-5">
-        <img src="/images/1980.jpeg" alt="Moamel Hashim" className="rounded-circle mb-3" width="200" />
+        <img src="public/images/1980.jpeg" alt="Moamel Hashim" className="rounded-circle mb-3" width="200" />
         <div className="">
         <p className="lead">Hello, I'm</p>
         <h1 className="display-4">Moamel Hashim</h1>
         <p className="fs-4 text-muted">Web developer</p>
         <div className="d-flex gap-3 justify-content-center">
-          <a className="btn btn-outline-secondary" href="/images/resume.pdf" target="_blank">Download CV</a>
+          <a className="btn btn-outline-secondary" href="public/images/resume.pdf" target="_blank">Download CV</a>
           <a className="btn btn-primary" href="#contact">Contact Info</a>
         </div>
         <div className="d-flex gap-3 mt-3 justify-content-center">
@@ -62,7 +62,7 @@ export default function Home() {
       </div>
       <div className="row align-items-center mt-5 justify-content-center">
         <div className="col-md-5 w-25 h-25">
-          <img src="/images/1994.jpeg" alt="About Me" className="img-fluid rounded" />
+          <img src="public/images/1994.jpeg" alt="About Me" className="img-fluid rounded" />
         </div>
         <div className="col-md-7">
           <div className="row mb-4">
@@ -78,8 +78,8 @@ export default function Home() {
               <div className="border rounded p-3 text-center h-100">
                 <i className="bi bi-people-fill display-6"></i>
                 <h5 className="fw-bold mt-2">Education</h5>
-                <p className="mb-0">B.Sc. Bachelors Degree</p>
-                <p className="text-muted">M.Sc. Masters Degree</p>
+                <p className="mb-0">A.S degree in web development</p>
+                <p className="text-muted">Software development boot camp</p>
               </div>
             </div>
           </div>
@@ -111,7 +111,7 @@ export default function Home() {
             <li className="nav-item"><a href="#projects" className="nav-link px-2">Projects</a></li>
             <li className="nav-item"><a href="#contact" className="nav-link px-2">Contact</a></li>
           </ul>
-          <p className="text-muted mb-0">&copy; 2023 John Doe. All Rights Reserved.</p>
+          <p className="text-muted mb-0">&copy; 2025 all rights reserved to Moamel Hashim</p>
         </div>
       </footer>
     </div>
